52d5bd0c031e68bd3b299bc54499c6109600d721
ElitsaY <eyotkova@gmail.com>
2024-10-06 17:04:16 +0300

    week 01 fix design mistake

--- Exercises/Solutions/01. Introduction/task01.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    int n;
-    std::cin >> n;
-    std::cout << n / 10 << std::endl;
-    return 0;
-}
--- Exercises/Solutions/01. Introduction/task09.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    int age;
-    std::cin >> age;
-    std::cout << "My age is " << age << std::endl;
-    return 0;
-}
